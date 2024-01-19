cc26d97729c6ecdf3282d6368801846dedc95da8
Адхам <adxamjon0806@gmail.com>
2024-01-19 15:25:03 +0500

    Cors Fixed

--- index.js
+++ index.js
@@ -21,7 +21,12 @@ const DB_URL =
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000/register",
+  })
+);
 app.use("", router);
 app.use(errorMiddleware);
 
